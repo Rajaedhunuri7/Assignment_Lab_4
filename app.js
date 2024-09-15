b6a7c0408112cf04c74d290646217bd524a7b18f
raj aedhunuri <62806954+Rajaedhunuri7@users.noreply.github.com>
2024-09-15 10:41:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const api = require("./api");
 const cors = require("cors");
+const middleware = require('./middleware')
+const bodyParser = require('body-parser')
 
 // Set the port
 const port = process.env.PORT || 3000;
@@ -10,14 +12,21 @@ const app = express();
 
 // Register the public directory
 app.use(express.static(__dirname + "/public"));
+app.use(middleware.cors)
+app.use(bodyParser)
+
+//register the routes
 app.get("/products/:id", api.getProducts);
+app.get("/products", api.listProducts);
+app.post('/products', api.createProducts);
+app.get('/', api.handleRoot);
+app.use(middleware.handleError)
+app.use(middleware.notFound)
 
 // Enable CORS
 app.use(cors());
 
-// Register the routes
-app.get("/products", api.listProducts);
-app.get("/", api.handleRoot);
+
 
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`));
