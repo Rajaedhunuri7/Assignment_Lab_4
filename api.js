6908c048c8e5bebf8b67cb65420c880489a7ea91
raj aedhunuri <62806954+Rajaedhunuri7@users.noreply.github.com>
2024-09-15 10:51:44 -0500

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
 const fs = require("fs").promises;
 const Products = require("./product");
-const path = require("path");
-
+const path = require("path")
 /**
  * Handle the root route
  * @param {object} req
@@ -10,23 +9,18 @@ const path = require("path");
 function handleRoot(req, res) {
     res.sendFile(path.join(__dirname, "/index.html"));
 }
-
 async function getProducts(req, res, next) {
     // Add CORS headers
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
+    //res.setHeader("Access-Control-Allow-Origin", "*");
     const { id } = req.params;
-
-    try {
-        const product = await Products.getProduct(id);
-
+    //try {
+        //const product = await Products.getProduct(id);
         if (!product) {
             return next(); // Handle not found case
         }
-
         res.json(product);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+    } //catch (err) {
+        //res.status(500).json({ error: err.message });
     }
 }
 
@@ -36,24 +30,30 @@ async function getProducts(req, res, next) {
  * @param {object} res
  */
 async function listProducts(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
+    //res.setHeader("Access-Control-Allow-Origin", "*");
     const { offset = 0, limit = 25 } = req.query;
 
-    try {
-        const products = await Products.list({
+   //try {
+        //const products = await Products.list({
+    res.json(await Products.list({
             offset: Number(offset),
             limit: Number(limit),
         });
 
-        res.json(products);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+        //res.json(products);
+    //} catch (err) {
+        //res.status(500).json({ error: err.message });
     }
 }
 
+
+async  function createProduct (req, res){
+    console.log('request body:', req.body)
+    res.json(req.body)
+}
 module.exports = {
     handleRoot,
     listProducts,
     getProducts,
+    createProducts
 };
