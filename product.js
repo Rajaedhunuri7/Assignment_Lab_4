857773b1325a5836e6d5eec167198de6b7836a68
raj aedhunuri <62806954+Rajaedhunuri7@users.noreply.github.com>
2024-09-15 08:31:54 -0500

    Create product.js

---
+++ product.js
@@ -0,0 +1,36 @@
+// products.js
+const fs = require("fs").promises;
+const path = require("path");
+const { urlToHttpOptions } = require("url");
+
+const productsFile = path.join(__dirname, "data/products.json");
+
+module.exports = {
+  list,
+  get,
+};
+
+/**
+ * List all products
+ * @returns {Promise<Array>}
+ */
+async function list() {
+  // const data = await fs.readFile(productsFile)
+  // return JSON.parse(data)
+  const { offset = 0, limit = 25 } = options;
+  const data = await fs.readFile(productsFile);
+  return JSON.parse(data).slice(offset, offset + limit);
+}
+
+async function get(id) {
+  const products = JSON.parse(await fs.readFile(productsFile));
+  // Loop through the products and return the product with the matching id
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === id) {
+      return products[i];
+    }
+  }
+
+  // If no product is found, return null
+  return null;
+}
